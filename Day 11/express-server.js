8d89a5c90671d964b8c7d472051b6398771f62ec
AHL96 <alexlopez841@gmail.com>
2018-05-26 20:28:19 -0400

    learn concept of taking input from front end

--- Day 11/express-server.js
+++ Day 11/express-server.js
@@ -1,13 +1,23 @@
 var express = require('express')
+var fs = require('fs')
+
 var app = express()
 
 app.get('/', function (req, res) {
-    res.send('Hello World!')
     res.send('Simple Example of routes!')
 })
 
 app.get('/signup', function (req, res) {
     res.send('This is a sign up page')
+
+    // this is how we will recieve params from the front end
+    var name = req.query.name;
+    var email = req.query.email;
+    var password = req.query.password;
+    console.log(name + ' ' + email + ' ' + password);
+
+    res.send('in signup module')
+
 })
 
 app.get('/signin', function (req, res) {
